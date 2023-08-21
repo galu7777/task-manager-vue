e917ae96967d9233b40b40829a2d671755c2e540
galu7777 <luisgalea31@gmail.com>
2023-08-21 10:32:28 -0400

    actualizando estilos

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,24 @@
-<script>
-
-</script>
-
 <template>
     <div>
         <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
             <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">Tasks Manager App</span>
+                <div class="ctn">
+                    <img src="../assets/check.png" alt="check" class="img">
+                    <span class="navbar-brand mb-0 h1">Tasks Manager App</span>
+                </div>
             </div>
         </nav>
     </div>
 </template>
 
 <style>
-
+    .ctn{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .img {
+        height: 2rem;
+        margin-right: 5px;
+    }
 </style>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -53,41 +53,45 @@
         <div class="row">
             <div class="col-lg-6 offset-lg-3">
                 <!-- Tareas Pendientes -->
-                <h2 class="title">Tareas Pendientes</h2>
-                <div class="card p-2" v-if="$store.state.Tasks.length === 0">
-                    <h6>No hay tareas pendientes para mostrar</h6>
+                <div class="card-container">
+                    <h2 class="title">Tareas Pendientes</h2>
+                    <div class="card p-2" v-if="$store.state.Tasks.length === 0">
+                        <h6>No hay tareas pendientes para mostrar</h6>
+                    </div>
+                    <ul class="list-group" v-for="(task, index) in $store.state.Tasks" :key="index">
+                        <li class="list-group-item d-flex justify-content-between">
+                            <span :class="[task.state === false ? 'text': 'text-success', 'cursor']" @click="updateTask(index)">
+                                <i 
+                                    :class="[task.state === false ? 'fa-regular fa-circle' : 'fa-solid fa-circle-check' ]">
+                                </i>
+                            </span>
+                            {{ task.name }}
+                            <span class="cursor text-danger" @click="deletedTask(index)">
+                                <i class="fa-solid fa-trash"></i>
+                            </span>
+                        </li>
+                    </ul>
                 </div>
-                <ul class="list-group" v-for="(task, index) in $store.state.Tasks" :key="index">
-                    <li class="list-group-item d-flex justify-content-between">
-                        <span :class="[task.state === false ? 'text': 'text-success', 'cursor']" @click="updateTask(index)">
-                            <i 
-                                :class="[task.state === false ? 'fa-regular fa-circle' : 'fa-solid fa-circle-check' ]">
-                            </i>
-                        </span>
-                        {{ task.name }}
-                        <span class="cursor text-danger" @click="deletedTask(index)">
-                            <i class="fa-solid fa-trash"></i>
-                        </span>
-                    </li>
-                </ul>
                 <!-- Tareas Completadas  -->
-                <h2 class="title mt-4">Tareas Completadas</h2>
-                <div class="card p-2" v-if="$store.state.TasksSuccess.length === 0">
-                    <h6>No hay tareas completadas para mostrar</h6>
+                <div class="card-container">
+                    <h2 class="title">Tareas Completadas</h2>
+                    <div class="card p-2" v-if="$store.state.TasksSuccess.length === 0">
+                        <h6>No hay tareas completadas para mostrar</h6>
+                    </div>
+                    <ul class="list-group" v-for="(task, index) in $store.state.TasksSuccess" :key="index">
+                        <li class="list-group-item d-flex justify-content-between">
+                            <span :class="[task.state === true ? 'text-success': 'text', 'cursor']" @click="updateTaskSuccess(index)">
+                                <i 
+                                    :class="[task.state === true ?'fa-solid fa-circle-check': 'fa-regular fa-circle' ]">
+                                </i>
+                            </span>
+                            {{ task.name }}
+                            <span class="cursor text-danger" @click="deletedTaskSuccess(index)">
+                                <i class="fa-solid fa-trash"></i>
+                            </span>
+                        </li>
+                    </ul>
                 </div>
-                <ul class="list-group" v-for="(task, index) in $store.state.TasksSuccess" :key="index">
-                    <li class="list-group-item d-flex justify-content-between">
-                        <span :class="[task.state === true ? 'text-success': 'text', 'cursor']" @click="updateTaskSuccess(index)">
-                            <i 
-                                :class="[task.state === true ?'fa-solid fa-circle-check': 'fa-regular fa-circle' ]">
-                            </i>
-                        </span>
-                        {{ task.name }}
-                        <span class="cursor text-danger" @click="deletedTaskSuccess(index)">
-                            <i class="fa-solid fa-trash"></i>
-                        </span>
-                    </li>
-                </ul>
             </div>
         </div>
     </div>
