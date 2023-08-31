4349536ebad677506486c69401e1598dc71953fa
galu7777 <luisgalea31@gmail.com>
2023-08-31 12:08:22 -0400

    feat<src>: add vue-router@4

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './routes' 
 import store from './store'
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
