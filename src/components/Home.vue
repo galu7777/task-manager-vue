5401b68bb8ff6d32383180a23c07e3205fed2de7
galu7777 <luisgalea31@gmail.com>
2023-08-31 12:09:30 -0400

    feat<src/components/Home>: module home

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Navbar from './NavBar.vue'
+import Task from './Task.vue'
+
+const navBar = Navbar
+// export default {
+//   name: 'App',
+//   components: {
+//     navBar: Navbar,
+//     Task
+//   }
+// }
+
+</script>
+
+<template>
+  <div class="fondo">
+    <navBar />
+    <Task />
+  </div>
+</template>
+
+<style>
+  .fondo {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    background-image: url('../assets/beach.jpg');
+    background-size: cover;
+    background-position: center;
+    }
+</style>
