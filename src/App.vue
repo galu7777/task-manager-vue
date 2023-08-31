1e6d6e8cd8425eb6bffe9fa9a5b2662ec2742ee1
galu7777 <luisgalea31@gmail.com>
2023-08-31 12:11:28 -0400

    fix<src>: update app

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,6 @@
 <script>
-import Navbar from './components/NavBar.vue'
-import Task from './components/Task.vue'
-
-export default {
-  name: 'App',
-  components: {
-    navBar: Navbar,
-    Task
-  }
-}
-
 </script>
 
 <template>
-  <div class="fondo">
-    <navBar />
-    <Task />
-  </div>
+  <RouterView/>
 </template>
-
-<style>
-  .fondo {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    background-image: url('./assets/beach.jpg');
-    background-size: cover;
-    background-position: center;
-    }
-</style>
