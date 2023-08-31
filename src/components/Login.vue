5d3f3914d0b843bba74fdbf30d5ee5a78ab5bed3
galu7777 <luisgalea31@gmail.com>
2023-08-31 12:10:45 -0400

    feat<src/componets/login>: add login

---
+++ src/components/Login.vue
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<template>
+    <h1>Mundo</h1>
+    <div>
+        <form action="">
+            <input type="password" name="" id="">
+            <input type="email" name="" id="">
+            <button>submit</button>
+        </form>
+    </div>
+</template>
