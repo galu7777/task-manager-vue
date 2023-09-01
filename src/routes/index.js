04d2fc1b2409cf86b7d311ac60ede5e44fa303d3
galu7777 <luisgalea31@gmail.com>
2023-09-01 17:25:51 -0400

    feat<src/routes>: making routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Login from '../components/Login.vue'
+import Register from '../components/Register.vue'
 import Home from '../components/Home.vue'
 
 const routes = [
     { path: '/', component: Login },
-    { path: '/home', component: Home },
-    { path: '/login', component: Login }
+    { path: '/home', component: Home, meta: { needsAuth: true } },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if(to.meta.needsAuth){
+        next('/login')
+    } else {
+        next()
+    }
 })
 
 export default router;
