5c3b00b0b1874cb245b688519c9c5bd4050cfd9e
galu7777 <luisgalea31@gmail.com>
2023-09-03 23:39:12 -0400

    feat<src>: add axios

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
@@ -6,42 +7,88 @@ export default createStore({
     TasksSuccess: [],
     nameTask: null,
     isUserLoggedIn: false,
-    user: {}
+    user: null,
+    token: ''
   },
   mutations: {
-    addTask(state) {
+    async addTask(state) {
+      const id = String(state.user[0].id)
       const task = {
+        userId: id,
         name: state.nameTask,
-        state: false
+        state: 'pending',
+        color: '#fff',
+        limitAt: "2023-08-24T19:05:13.519-04:00"
       };
-      state.Tasks.push(task);
-      state.nameTask = '';
+      const token = state.token
+      const resp = await axios.post('http://localhost:3333/tasks', task, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      })
+      state.Tasks.push(resp.data)
+      console.log(state.Tasks)
+      // state.Tasks.push(resp.data)
     },
-    deletedTask(state, index) {
-      console.log(index)
-      state.Tasks.splice(index, 1);
+    async deletedTask(state, index) {
+      const task = state.Tasks.splice(index, 1);
+      const token = state.token
+      const resp = await axios.delete(`http://localhost:3333/tasks/${task[0].id}`, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      })
+      console.log(resp.data)
     },
-    deletedTaskSuccess(state, index) {
-      state.TasksSuccess.splice(index, 1);
+    async deletedTaskSuccess(state, index) {
+      const task = state.TasksSuccess.splice(index, 1);
+      const token = state.token
+      const resp = await axios.delete(`http://localhost:3333/tasks/${task[0].id}`, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      })
+      console.log(resp.data)
     },
-    updateTask(state, index){
-      state.Tasks[index].state = !state.Tasks[index].state
-      const task = state.Tasks[index]
-      state.TasksSuccess.push(task)
-      state.Tasks.splice(index, 1)
+    async updateTask(state, index){
+        const task = state.Tasks[index]
+        const data = { state: 'done' }
+        const token = state.token
+        const resp = await axios.put(`http://localhost:3333/tasks/${task.id}`, data, {
+          headers: { 'Authorization': `Bearer ${token}` }
+        })
+        console.log(resp.data.state)
+        state.TasksSuccess.push(task)
+        state.Tasks.splice(index, 1)
     },
-    updateTasksSuccess(state, index) {
-      state.TasksSuccess[index].state = false
+    async updateTasksSuccess(state, index) {
+      const task = state.TasksSuccess[index]
+      const data = { state: 'pending' }
+        const token = state.token
+        const resp = await axios.put(`http://localhost:3333/tasks/${task.id}`, data, {
+          headers: { 'Authorization': `Bearer ${token}` }
+        })
       const taskSuccess = state.TasksSuccess[index]
       state.Tasks.push(taskSuccess)
       state.TasksSuccess.splice(index, 1)
+      console.log(resp.data.state)
     },
-    isLogin(state, user){
-      console.log(user)
-      if(state.isUserLoggedIn === false) state.isUserLoggedIn = true
-      state.user = user
-      console.log(state.user)
-    }    
+    async isLogin(state, user){
+      try {
+        const resp = await axios.post('http://localhost:3333/login', user)
+        const token = resp.data.token
+        state.token = token
+        const client = await axios('http://localhost:3333/users', {
+          headers: { 'Authorization': `Bearer ${token}` }
+        })
+        const userFound = client.data.filter((data) => data.nickName === user.nick_name)
+        const tasksPeding = userFound[0].task.filter((data) => data.state === 'pending')
+        const tasksSuccess = userFound[0].task.filter((data) => data.state === 'done')
+        state.user = userFound
+        if(tasksPeding) state.Tasks = tasksPeding
+        if(taskSuccess) state.TasksSuccess = tasksSuccess
+      } catch (error) {
+        console.log('error: ', error)
+      }
+    },
+    isLogout(state){
+      state.token = ''
+      console.log(state.token)
+    }
   },
   actions: {
     addTaskAction(context) {
@@ -61,6 +108,9 @@ export default createStore({
     },
     isLogin(context, user) {
       context.commit('isLogin', user);
+    },
+    isLogout(context) {
+      context.commit('isLogout')
     }
   }
 });
