f5192e7d9c31799e3a5abe4f0388d13634bbb1fb
galu7777 <luisgalea31@gmail.com>
2023-09-14 21:53:51 -0400

    feat<src/components>: doing to configuser

---
+++ src/components/ConfigUser.vue
@@ -0,0 +1,17 @@
+<script>
+  import Navbar from './NavBar.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            navBar: Navbar
+        }
+    }
+</script>
+
+<template>
+    <div class="fondo">
+        <navBar/>
+        <h1>Config</h1>
+    </div>
+</template>
