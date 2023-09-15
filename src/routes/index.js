161710662b844fc3d1c68b812773363212b4ab8b
galu7777 <luisgalea31@gmail.com>
2023-09-14 21:55:41 -0400

    feat<src/routes>: add path config

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Login from '../components/Login.vue'
 import Register from '../components/Register.vue'
+import ConfigUser from '../components/ConfigUser.vue'
 import Home from '../components/Dashboard.vue'
 import store from '../store'
 
 const routes = [
     { path: '/', name: 'root', component: Login },
     { path: '/home', name: 'home', component: Home, meta: { needsAuth: true } },
+    { path: '/config', name: 'config-user', component: ConfigUser, meta: { needsAuth: true } },
     { path: '/login', name: 'login', component: Login },
     { path: '/register', name: 'register', component: Register }
 ]
