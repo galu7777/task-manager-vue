bc799cc2913e5f079ee7c1d8a8bc05a661b8bf7a
galu7777 <luisgalea31@gmail.com>
2023-09-21 08:50:13 -0400

    fix<src/helpers>: separating tasks by status

--- src/helpers/storeHelpersUsers.js
+++ src/helpers/storeHelpersUsers.js
@@ -1,4 +1,4 @@
-import { apiLogin, apiGetUsers, apiPostUser } from '../api/users'
+import { apiLogin, apiPostUser, apiGetUserTasks } from '../api/users'
 import router from '../routes/index'
 
 export const userregister = async (state) => {
@@ -21,14 +21,12 @@ export const islogin = async (state, user) => {
         const resp = await apiLogin(user)
         const token = resp.data.token
         state.token = token
-        const client = await apiGetUsers(token)
-        const userFound = client.data.filter((data) => data.nickName === user.nick_name || data.email === user.nick_name)
-        const tasksPending = userFound[0].task.filter((data) => data.state === 'pending')
-        const tasksSuccess = userFound[0].task.filter((data) => data.state === 'done')
+        const client = await apiGetUserTasks(token)
+        const tasksPending = client.data.data.tasks.filter((data) => data.state === 'pending')
+        const tasksSuccess =client.data.data.tasks.filter((data) => data.state === 'done')
         localStorage.token = JSON.stringify(token)
         localStorage.tasksPending = JSON.stringify(tasksPending)
         localStorage.tasksSuccess = JSON.stringify(tasksSuccess)
-        state.user = userFound
         state.Tasks = tasksPending
         state.TasksSuccess = tasksSuccess
         router.push('/home')
