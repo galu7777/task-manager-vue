8197bd39e9c7a2315aeceb2f47126f7a6bf0fcf4
galu7777 <luisgalea31@gmail.com>
2023-09-30 09:21:51 -0400

    feat<src/components>: implemente component vuetify

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,11 @@
 <script>
     import SideBar from './SideBar.vue'
     export default {
+        data () {
+            return {
+                drawer: null,
+            }
+        },
         methods: {
             logout() {
                 this.$store.dispatch('isLogout')
@@ -12,19 +17,54 @@
     }
 </script>
 <template>
-    <div>
-        <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-            <div class="container-fluid">
-                <div class="ctn">
-                    <img src="../assets/check.png" alt="check" class="img">
-                    <span class="navbar-brand mb-0 h1">Tasks Manager App</span>                    
-                </div>                
-                <div>
-                    <sideBar />
-                </div>
-            </div>
-        </nav>        
-    </div>    
+    <v-layout>
+      <v-app-bar
+        color="#313843"
+        density="compact"
+      >
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon><img src="../assets/check.png" alt="check" class="img"></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>Tasks Manager App</v-app-bar-title>
+
+        <template v-slot:append>
+            <span class="cursor text-danger" @click.stop="drawer = !drawer">
+                <i class="fa-solid fa-bars fa-size" style="color: #edeff2;"></i>
+            </span>
+        </template>
+      </v-app-bar>
+      <v-navigation-drawer
+        v-model="drawer"
+        temporary
+        color="#313843"
+      >
+        <v-list density="compact" nav>
+          <v-list-item>
+            <span class="cursor">          
+                <router-link to="/home" class="list-group-item">
+                    <i class="fa-solid fa-house fa-size-md" style="color: #fff;"></i> Home
+                </router-link>
+            </span>
+          </v-list-item>
+          <v-list-item>
+            <span>
+                <router-link to="/config" class="list-group-item">
+                    <i class="fa-solid fa-gear fa-size-md" style="color: #fff;"></i> Configuraciones
+                </router-link>
+            </span>
+          </v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+          <v-list-item>
+            <span class="cursor text-danger" @click.prevent="logout">
+              <i class="fa-solid fa-right-from-bracket fa-size"></i>
+            </span>
+          </v-list-item>
+      </v-navigation-drawer>
+           
+    </v-layout>
+  <!-- </v-card> -->
 </template>
 
 <style>
@@ -36,5 +76,8 @@
     .img {
         height: 2rem;
         margin-right: 5px;
+    }
+    .fa-size {
+    font-size :2em;
     }
 </style>
