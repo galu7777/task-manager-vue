f80a169aee3ba0fe1f9a42bc754c0879f51f70b0
galu7777 <luisgalea31@gmail.com>
2023-09-30 09:20:05 -0400

    feat<src/components>: add configuser

--- src/components/ConfigUser.vue
+++ src/components/ConfigUser.vue
@@ -1,10 +1,17 @@
 <script>
   import Navbar from './NavBar.vue'
-
     export default {
         name: 'App',
         components: {
             navBar: Navbar
+        },
+        data(){
+            return {
+                user: {
+                    nick_name: '',
+                    email: ''
+                }
+            }
         }
     }
 </script>
@@ -12,6 +19,34 @@
 <template>
     <div class="fondo">
         <navBar/>
-        <h1>Config</h1>
+        <div class="container">
+          <div class="bg-container">
+            <h1>Hola</h1>
+            <ul>
+                <li></li>
+            </ul>
+            <input type="text">
+            <input type="text">
+            <input type="text">
+          </div>
+        </div>
     </div>
 </template>
+
+<style>
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 25%;
+    }
+    .bg-container{
+        display: flex;
+        flex-direction: row;
+        /* justify-content: center;
+        align-items: center; */
+        background: #fff;
+        height: 50%;
+        width: 50%;
+    }
+</style>
