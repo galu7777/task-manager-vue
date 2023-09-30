4db65239306accfa5b7e2dff1d373e0e7b858913
galu7777 <luisgalea31@gmail.com>
2023-09-30 09:17:28 -0400

    feat<>: implemented vuetify

--- src/main.js
+++ src/main.js
@@ -4,5 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes' 
 import store from './store'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).use(router).use(store).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).use(router).use(store).mount('#app')
