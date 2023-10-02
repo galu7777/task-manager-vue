d94035e085233868c8e48ecceced2c710563ea1b
galu7777 <luisgalea31@gmail.com>
2023-10-02 00:28:09 -0400

    feat<src/components>: changepassworduser reinfouse

--- src/helpers/storeHelpersUsers.js
+++ src/helpers/storeHelpersUsers.js
@@ -1,6 +1,8 @@
 import { apiLogin, apiPostUser, apiGetUserTasks, getUser } from '../api/users'
-import { login, loginError } from './modalSwal'
+import { errorSwal, login, loginError } from './modalSwal'
+import { changePasswordErrorSwal, changePasswordSwal } from './modalSwalUser'
 import router from '../routes/index'
+import store from '../store'
 
 export const userregister = async (state) => {
     try {
@@ -21,7 +23,7 @@ export const islogin = async (state, user) => {
     try {
         const resp = await apiLogin(user)
         const token = resp.data.data.token
-        const userResp = await getUser(token) 
+        const userResp = await getUser(token)
         state.token = token
         const client = await apiGetUserTasks(token)
         const tasksPending = client.data.data.tasks.filter((data) => data.state === 'pending')
@@ -29,14 +31,44 @@ export const islogin = async (state, user) => {
         localStorage.token = JSON.stringify(token)
         state.Tasks = tasksPending
         state.TasksSuccess = tasksSuccess
-        state.user = userResp.data
+        state.userData = userResp.data.data
         login(userResp.data)
         router.push('/home')
-        console.log(client.data)
       } catch (error) {
         loginError()
         console.log('error: ', error)
       }
+}
+
+export const changePasswordUser = async (state, user) => {
+  try {
+    const token = state.token
+    if(user.newpassword === user.repeatnewpassword){
+      const password = {
+        oldpassword: user.oldpassword,
+        newpassword: user.newpassword
+      }
+      const resp = await changePasswordSwal(password, token)
+      console.log(resp)
+      if(resp.data.data.nick_name) {
+        router.push('/')
+      }
+      else changePasswordErrorSwal()
+    } else if(user.newpassword !== user.repeatnewpassword){
+      errorSwal('Las contraseña Nueva no son Iguales')
+    }
+  } catch (error) {
+    console.log('error: ', error)
+  }
+}
+
+export const reInfoUser = async (token) => {
+  try {
+    const userResp = await getUser(token)
+    store.state.userData = userResp.data.data
+  } catch (error) {
+    console.log('error: ', error)
+  }
 }
 
 export const logout = (state) => {
