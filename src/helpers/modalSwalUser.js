8d7ee7b1e83d27ad1a6781a776e44b7ae3b81af3
galu7777 <luisgalea31@gmail.com>
2023-10-02 12:31:15 -0400

    feat<src/helpers>: changenameuserswal

--- src/helpers/modalSwalUser.js
+++ src/helpers/modalSwalUser.js
@@ -1,5 +1,6 @@
 import Swal from 'sweetalert2'
-import { apiChangePassword } from '../api/users'
+import { apiChangePassword, apiPutUser } from '../api/users'
+import store from '../store';
 // import store from '../store'
 
 export const changePasswordSwal = (password, token) => {
@@ -31,7 +32,39 @@ export const changePasswordSwal = (password, token) => {
       }
     });
   }
-  
+
+  export const changeNameUserSwal = (user, token) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const nick_name = store.state.userData.user.nick_name
+        const result = await Swal.fire({
+          title: 'Deseas cambiar el nombre de usuario.',
+          text: `Estás seguro que deseas cambiar el nombre de usuario "${nick_name}"?`,
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Sí, Cambiar'
+        });
+        
+        if (result.isConfirmed) {
+          const resp = await apiPutUser(user, token)
+          const newName = resp.data.data.nick_name
+          console.log(newName)
+          Swal.fire(
+            'Cambio de nombre de usuario exitoso!',
+            `Tu nombre de usuario fue cambiado exitosamente, por: "${newName}".`,
+            'success'
+          );
+          resolve(resp);
+        } else {
+          reject(new Error('Cambio de contraseña cancelado'));
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }  
 
 export const changePasswordErrorSwal = () => {
     Swal.fire({
